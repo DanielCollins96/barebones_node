3b6dd41bf88c3f89086dcc4506d359386f16259c
dc <danielmcollins1996@gmail.com>
2020-05-07 12:50:13 -0600

    Its all fucked now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const bcrypt = require('bcryptjs');
 const flash = require('connect-flash');
 
 let app = express();
@@ -39,42 +40,62 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     console.log(users.length)
-    if (users.length > 0) {
-        for (let user of users) {
-            console.log(user)
-            if (req.body.username == user.username && req.body.password == user.password) {
-                req.session.Authenticated = true;
-                req.session.user = req.body.username;
-                return res.render('profile', {Authenticated: req.session.Authenticated});
-            }
-        }
+    password = req.body.password;
+    for (let user of users) {
+        console.log('hi')
+            bcrypt.compare(password, user.password)
+                .then(match => {
+                    console.log('matched')
+                    if (match) {
+                        console.log('logged in')
+                        req.session.Authenticated = true;
+                        req.session.user = req.body.username;
+                        return res.redirect('/profile');
+                    } else {
+
+                        req.flash('error_msg', 'Username or Password incorrect');
+                        return res.redirect('/login')
+                    }        
+                })
+                .catch(error => console.log('error' + error))
+            
     }
+    console.log('2 time?')
     req.flash('error_msg', 'Username or Password incorrect');
-    res.render('login', {Authenticated: req.session.Authenticated})
+    res.redirect('/login')
+    // res.render('login', {Authenticated: req.session.Authenticated})
 })
 
 app.get('/register', (req, res) => {
     res.render('register', {Authenticated: req.Authenticated})
 })
 
-app.post('/register', (req, res) => {
-    for (let user of users) 
-        if (req.body.username == user.username)
-            return res.render('register', {error: "username already exists", Authenticated: req.session.Authenticated})
+app.post('/register', async (req, res) => {
+    for (let user of users) {
+        if (req.body.username == user.username) {
+            req.flash('error_msg', 'Username already exists');
+            return res.redirect('/register')
+            // return res.render('register', {Authenticated: req.session.Authenticated})
+        }
+    }
+    let hashedPassword = await bcrypt.hash(req.body.password, 10);
     let user = {
         username: req.body.username,
-        password: req.body.password
+        password: hashedPassword
     }
     users.push(user)
+    console.log('rererererere')
     req.flash('success_msg', 'You are now registered and can log in');
-    res.redirect('/login')
+    res.redirect('login');
+    // res.render('login', {Authenticated: req.session.Authenticated})
 })
 
 app.get('/profile', (req, res) => {
     if (req.session.Authenticated) {
         return res.render('profile', {Authenticated: req.session.Authenticated})
     }
-    res.render('login', {error: "Login to see your profile", Authenticated: req.session.Authenticated})
+    req.flash('error_msg', 'Login to see your profile')
+    res.render('login', {Authenticated: req.session.Authenticated})
 })
 
 app.get('/logout', (req, res) => {
