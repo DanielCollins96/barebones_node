5aea5170ae8ab4d71cfa1dbef51e39f4d49fbeae
Daniel Collins <danielmcollins1996@gmail.com>
2020-05-09 14:08:57 -0700

    bootstrap navbar and chart on homepage

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ let Post = require('./models/post');
 
 let app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
